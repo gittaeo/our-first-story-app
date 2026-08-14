73f79952049763c6a9fe8e9ce6ed5a360dfa1a6c
gittaeo <plus4567@naver.com>
2026-08-14 15:24:20 +0900

    Deploy Vite app to GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,2 @@
-import React from 'react';import{createRoot}from'react-dom/client';import{BrowserRouter}from'react-router-dom';import{Store}from'./store';import App from'./App';import'./styles.css';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><Store><App/></Store></BrowserRouter></React.StrictMode>);
+import React from 'react';import{createRoot}from'react-dom/client';import{HashRouter}from'react-router-dom';import{Store}from'./store';import App from'./App';import'./styles.css';
+createRoot(document.getElementById('root')!).render(<React.StrictMode><HashRouter><Store><App/></Store></HashRouter></React.StrictMode>);
